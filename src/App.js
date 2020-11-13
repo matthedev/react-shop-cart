c2730152f436a56e5eaa4785fcee2b5c8a8e3a62
matthedev <mtuxiashvili@yahoo.com>
2020-11-13 06:18:56 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Header from "./Components/Header/Header"
 import Main from "./Components/Main/Main"
 import Footer from "./Components/Footer/Footer"
 
+
+
+
+
 function App() {
   return (
     <div >
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import data from "../../data.json"
+import "./main.css"
 
-class Main extends Component {
-    render() {
-        return (
-            <div>
-                <span>probably many cards here!</span>
+// const Content = () => {
+//     const [products, setProducts] = useState(data.products)
+//     const [size, setSize] = useState("")
+//     const [sort, setSort] = useState("")
+//   }
+
+const Main = () => {
+
+  
+    return (
+        <div className="content">
+            <div className="main">
+               {data.products.map((product) => {
+                   return <div key={product._id}>
+                       <div className="product">
+                           <div className="background-img-wrapper">
+
+                           <div className="background-img"  style={{
+                                            backgroundImage: `url(${product.image})`
+                                        }} ></div>
+                           </div>
+                           {/* <img src={product.image} alt={product.title}/> */}
+                                 <a href="/">{product.title}</a>
+                                 <div className="product-detail">
+                                    <div className="product-price">{"$" + Number(product.price.toFixed(1)).toLocaleString()}</div>
+                                    <button className="primaryBtn">Add to Cart</button>
+                                 </div>
+                             
+                       </div>
+                   </div>
+               })}
             </div>
-        );
-    }
-}
+            <div className="sidebar">Cart Items</div>
+        </div>
+    );
+};
 
 export default Main;
